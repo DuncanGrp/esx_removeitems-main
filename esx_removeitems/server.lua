b95603617404ae9cfd022ef7d1dfc0ab5d43e506
DuncanGrp <137178299+DuncanGrp@users.noreply.github.com>
2023-06-20 16:35:56 +0100

    Update server.lua

--- esx_removeitems/server.lua
+++ esx_removeitems/server.lua
@@ -1,6 +1,7 @@
--- Registering an event for player respawn
-AddEventHandler('esx:onPlayerSpawn', function(playerId)
+-- Registering an event for player spawn
+AddEventHandler('playerSpawned', function(spawn)
     -- Getting the player's source
+    local playerId = source
     local playerPed = GetPlayerPed(playerId)
     
     -- Checking if the player's Ped is valid
